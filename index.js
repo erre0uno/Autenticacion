4946f047eb68c605825a8b9adda16aeac0bdc0c8
Nelson Andres Montealegre_乇яяє <nel.andres01@gmail.com>
2026-02-16 09:46:16 -0500

    Add Express server setup with Swagger documentation

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+const app = express();
+const PORT = 3000;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
